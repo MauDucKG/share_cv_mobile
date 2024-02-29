5e9f7c5c703a75116ffc46ca9299e11bf37d79f1
nguye <nguyenmauduc91@gmail.com>
2024-02-29 21:39:36 +0700

    duc: improve webview

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,34 @@
+import { SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function TabOneScreen() {
+  const runFirst = `
+      setTimeout(function() { 
+        var elements = document.getElementsByClassName('css-gz8ypf');
+        for (var i = 0; i < elements.length; i++) {
+          elements[i].style.display = 'none';
+        }
+        var elements2 = document.getElementsByClassName('css-a2x32k');
+      for (var i = 0; i < elements2.length; i++) {
+        elements2[i].style.display = 'none';
+      }
+        });
+      true; // note: this is required, or you'll sometimes get silent failures
+    `;
+
+  const runBeforeFirst = `
+      window.isNativeApp = true;
+      true; // note: this is required, or you'll sometimes get silent failures
+  `;
+
   return (
-    <WebView
-      source={{ uri: 'https://share-cv.vercel.app/' }}
-      style={{ flex: 1 }}
-      javaScriptEnabled
-    />
+    <SafeAreaView style={{ flex: 1 }}>
+      <WebView
+        source={{ uri: 'https://share-cv.vercel.app/' }}
+        onMessage={(event) => { }}
+        injectedJavaScript={runFirst}
+        injectedJavaScriptBeforeContentLoaded={runBeforeFirst}
+      />
+    </SafeAreaView>
   );
 }
